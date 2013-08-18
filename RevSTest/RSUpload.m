fb84d513c3e235ef14c4d1a78cadf0c3300f5f1a
theGreatLzbdd <the.great.lzbdd@gmail.com>
2013-08-18 17:29:09 +0800

    Added GNU LGPL

--- RevSTest/RSUpload.m
+++ RevSTest/RSUpload.m
@@ -2,9 +2,26 @@
 //  RSUpload.m
 //  RevS
 //
-//  Created by lzbdd on 13-8-1.
-//  Copyright (c) 2013年 lzbdd. All rights reserved.
-//
+//  Created by Zebang Liu on 13-8-1.
+//  Copyright (c) 2013年 Zebang Liu. All rights reserved.
+//  Contact: the.great.lzbdd@gmail.com
+/*
+ This file is part of RevS.
+ 
+ RevS is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ RevS is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with RevS.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #import "RevS.h"
 
@@ -35,12 +52,20 @@
     
     for (NSUInteger i = 0; i < K_NEIGHBOUR; i++) {
         if (i < contactList.count) {
-            NSString *messageString = [NSString stringWithFormat:@"UFILE_%@;%@;%d",fileName,[RSUtilities getLocalIPAddress],TTL];
+            NSString *messageString = [NSString stringWithFormat:@"UFILE_%@;%@;%ld",fileName,[RSUtilities getLocalIPAddress],(unsigned long)TTL];
             RSMessager *message = [RSMessager messagerWithPort:UPLOAD_PORT];
             [message addDelegate:[RSListener sharedListener]];
             [message sendTcpMessage:messageString toHost:[contactList objectAtIndex:i] tag:0];
         }
     }
+}
+
++ (void)uploadFile:(NSString *)fileName toHost:(NSString *)host
+{
+    NSString *messageString = [NSString stringWithFormat:@"SENDFILE_%@;%@;%d;%@",fileName,[NSData dataWithContentsOfFile:[STORED_DATA_DIRECTORY stringByAppendingString:fileName]],0,[RSUtilities getLocalIPAddress]];
+    RSMessager *message = [RSMessager messagerWithPort:UPLOAD_PORT];
+    [message addDelegate:[RSListener sharedListener]];
+    [message sendTcpMessage:messageString toHost:host tag:0];
 }
 
 - (void)addDelegate:(id <RSUploadDelegate>)delegate
